b1acee8b46f4112ac259d5c4b1ddaaf4933cc79b
Austin Whisnant <abwhisnant@cert.org>
2024-11-20 10:17:34 -0500

    License updates

--- development/generate_example_md.py
+++ development/generate_example_md.py
@@ -1,4 +1,14 @@
-'''Generates markdown files from json schema'''
+'''Generates markdown files from json schema
+
+## Licensing
+This file is a part of the Insider Incident Data Exchange Standard (IIDES) - see [https://github.com/cmu-sei/IIDES](https://github.com/cmu-sei/IIDES)
+Copyright 2024 Carnegie Mellon University.
+[DISTRIBUTION STATEMENT A] This material has been approved for public release and unlimited distribution. Please see Copyright notice for non-US Government use and distribution.
+This work is provided "AS-IS" with "NO WARRANTIES OF ANY KIND - EXPRESS OR IMPLIED" and is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
+Requests for permission for non-licensed uses should be directed to the Software Engineering Institute at permission@sei.cmu.edu.
+CERT® is registered in the U.S. Patent and Trademark Office by Carnegie Mellon University.
+DM24-0776
+'''
 import json
 import os
 import sys
--- development/generate_schema_doc.py
+++ development/generate_schema_doc.py
@@ -9,6 +9,14 @@ large file. The schema file can them be used offline.
 *** COMMON REFERENCES ARE BROKEN ***
 ** MIGHT BE MISSING SOME REFERENCES...
 
+## Licensing
+This file is a part of the Insider Incident Data Exchange Standard (IIDES) - see [https://github.com/cmu-sei/IIDES](https://github.com/cmu-sei/IIDES)
+Copyright 2024 Carnegie Mellon University.
+[DISTRIBUTION STATEMENT A] This material has been approved for public release and unlimited distribution. Please see Copyright notice for non-US Government use and distribution.
+This work is provided "AS-IS" with "NO WARRANTIES OF ANY KIND - EXPRESS OR IMPLIED" and is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
+Requests for permission for non-licensed uses should be directed to the Software Engineering Institute at permission@sei.cmu.edu.
+CERT® is registered in the U.S. Patent and Trademark Office by Carnegie Mellon University.
+DM24-0776
 """
 import os
 import json
